618252ad2245913f48e0089eb363d26bcc687b89
Edigar Herculano <edigarhdev@gmail.com>
2024-03-02 16:27:11 -0300

    test: increase usecase post coverage (#26)

--- internal/usecase/mock/mock_post_repositrory.go
+++ internal/usecase/mock/mock_post_repositrory.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"database/sql"
+	"errors"
 	"github.com/edigar/socialnets-api/internal/entity"
 )
 
@@ -11,7 +12,8 @@ func NewMockPostRepository() *MockPostRepository {
 	return &MockPostRepository{}
 }
 
-const NEW_POST_ID = 3
+const NEW_POST_ID = 4
+const ERROR = "ERROR"
 
 var MockPosts = []entity.Post{
 	{
@@ -25,6 +27,13 @@ var MockPosts = []entity.Post{
 		Id:       2,
 		Title:    "Title 2",
 		Content:  "Content 2",
+		AuthorId: "d9b56fd4-31b7-4bd5-958f-99028ca5e79a",
+		Likes:    0,
+	},
+	{
+		Id:       3,
+		Title:    "Title 3",
+		Content:  "Content 3",
 		AuthorId: "d9b56fd4-31b7-4bd5-958f-99028ca5e79a",
 		Likes:    0,
 	},
@@ -45,7 +54,11 @@ func (mr MockPostRepository) FetchById(postId uint64) (entity.Post, error) {
 }
 
 func (mr MockPostRepository) FetchByUser(userId string) ([]entity.Post, error) {
-	return nil, nil
+	if userId == ERROR {
+		return nil, errors.New("driver: bad connection")
+	}
+
+	return MockPosts, nil
 }
 
 func (mr MockPostRepository) Update(postId uint64, post entity.Post) error {
@@ -60,17 +73,54 @@ func (mr MockPostRepository) Update(postId uint64, post entity.Post) error {
 }
 
 func (r MockPostRepository) Delete(postId uint64) error {
-	return nil
+	index := 99
+	for i, post := range MockPosts {
+		if postId == post.Id {
+			index = i
+		}
+	}
+
+	if index != 99 {
+		MockPosts = append(MockPosts[:index], MockPosts[index+1:]...)
+		return nil
+	}
+
+	return sql.ErrNoRows
 }
 
 func (mr MockPostRepository) FetchUserPosts(userId string) ([]entity.Post, error) {
-	return nil, nil
+	if userId == ERROR {
+		return nil, errors.New("driver: bad connection")
+	}
+
+	var posts []entity.Post
+	for _, post := range MockPosts {
+		if post.AuthorId == userId {
+			posts = append(posts, post)
+		}
+	}
+
+	return posts, nil
 }
 
 func (mr MockPostRepository) LikePost(postId uint64) error {
-	return nil
+	for i, post := range MockPosts {
+		if postId == post.Id {
+			MockPosts[i].Likes++
+			return nil
+		}
+	}
+
+	return sql.ErrNoRows
 }
 
 func (mr MockPostRepository) UnlikePost(postId uint64) error {
-	return nil
+	for i, post := range MockPosts {
+		if postId == post.Id && post.Likes > 0 {
+			MockPosts[i].Likes--
+			return nil
+		}
+	}
+
+	return sql.ErrNoRows
 }
