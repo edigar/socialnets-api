6c2a1f5571b9a05c79ce858946458417c4ff0228
Edigar Herculano <edigarhdev@gmail.com>
2024-04-03 16:47:20 -0300

    feat: omit json marshal password (#33)

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -15,7 +15,7 @@ type User struct {
 	Name      string     `json:"name,omitempty"`
 	Nick      string     `json:"nick,omitempty"`
 	Email     string     `json:"email,omitempty"`
-	Password  string     `json:"password,omitempty"`
+	Password  string     `json:"-"`
 	CreatedAt time.Time  `json:"createdAt,omitempty"`
 	UpdatedAt *time.Time `json:"updatedAt,omitempty"`
 }
